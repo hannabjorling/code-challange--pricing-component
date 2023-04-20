d4d129609f5a0e7ff8ec8b61d514e42b83f126e8
Hanna Björling <hanna.bjorling@hotmail.com>
2023-04-20 17:24:51 +0200

    added ts to change the prices

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,23 @@
 import './styles.css'
 
+const slider = document.querySelector('input[type=checkbox]') as HTMLElement
+
+slider.addEventListener('click', () => {
+    // read current prices showing
+    const price = document.querySelector('.price')
+    const basicEl = document.querySelector('#basic-price')
+    const professionalEl = document.querySelector('#prefessional-price')
+    const masterEl = document.querySelector('#master-price')
+
+    // annually = default, switch to monthly
+    if (price?.innerHTML == '$199.99') {
+        basicEl!.innerHTML = `&dollar;19.99`
+        professionalEl!.innerHTML = `&dollar;24.99`
+        masterEl!.innerHTML = `&dollar;39.99`
+    } else if (price?.innerHTML === '$19.99') {
+        basicEl!.innerHTML = `&dollar;199.99`
+        professionalEl!.innerHTML = `&dollar;249.99`
+        masterEl!.innerHTML = `&dollar;399.99`
+    }
+})
+
